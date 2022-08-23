2e7a90d43076344c250fb562704ad000545910aa
Rich Neswold <neswold@fnal.gov>
2022-08-23 15:02:19 -0500

    :art: improve layout and formatting of UI

--- lib/main.dart
+++ lib/main.dart
@@ -66,10 +66,12 @@ class PlotWidget extends StatelessWidget {
 
     return Center(
         child: Row(
-      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
-        Text(label),
-        TextButton(
+        Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Text(label, style: Theme.of(context).textTheme.headline6)),
+        ElevatedButton(
           child: const Text('Stop Plot'),
           onPressed: () {
             state.clearDevice();
@@ -88,16 +90,16 @@ class QueryWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        const Text('Device to plot:'),
-        Expanded(child: TextField(
-          onSubmitted: (String name) {
-            state.setDevice(name);
-          },
-        ))
-      ],
-    ));
+        child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: TextField(
+              decoration: const InputDecoration(
+                  labelText: 'Device Name*', border: OutlineInputBorder()),
+              onSubmitted: (String name) {
+                if (!name.isEmpty) {
+                  state.setDevice(name.toUpperCase());
+                }
+              },
+            )));
   }
 }
