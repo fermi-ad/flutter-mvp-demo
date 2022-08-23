71d1cb44715bd8552a0c15e96ff64cc8b1514bfe
Rich Neswold <neswold@fnal.gov>
2022-08-23 16:10:53 -0500

    :sparkles: plot data
    This commit pulls in a Flutter Plot package. The data is random (instead of
    reading the actual device entered by the user.)

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+import "dart:async";
+import "dart:math";
+
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -55,30 +59,81 @@ class RootState extends State<RootWidget> {
   }
 }
 
-class PlotWidget extends StatelessWidget {
+class PlotWidget extends StatefulWidget {
   const PlotWidget({Key? key, required this.state}) : super(key: key);
 
   final RootState state;
 
   @override
+  _ChartState createState() => _ChartState(state);
+}
+
+class _ChartState extends State<PlotWidget> {
+  _ChartState(this.state);
+
+  final RootState state;
+
+  final pointLimit = 40;
+  final points = <FlSpot>[const FlSpot(0.0, 70.0)];
+
+  double xValue = 0.0;
+  double yValue = 70.0;
+  late Timer timer;
+
+  @override
+  void initState() {
+    super.initState();
+
+    timer = Timer.periodic(const Duration(milliseconds: 1000), (timer) {
+      setState(() {
+        xValue = xValue + 1.0;
+        yValue += (10.0 * Random().nextDouble()).floorToDouble() / 10.0 - 0.5;
+        points.add(FlSpot(xValue, yValue));
+        if (points.length > pointLimit) {
+          points.removeAt(0);
+        }
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     var label = 'Plotting ${state._device}';
 
-    return Center(
-        child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: Text(label, style: Theme.of(context).textTheme.headline6)),
-        ElevatedButton(
-          child: const Text('Stop Plot'),
-          onPressed: () {
-            state.clearDevice();
-          },
-        )
-      ],
-    ));
+    return points.isNotEmpty
+        ? Center(
+            child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(label,
+                      style: Theme.of(context).textTheme.headline6)),
+              SizedBox(
+                  width: 640,
+                  height: 480.0,
+                  child: LineChart(LineChartData(
+                      minY: 50.0,
+                      maxY: 90.0,
+                      minX: points.first.x,
+                      maxX: points.first.x + pointLimit - 1,
+                      clipData: FlClipData.all(),
+                      lineBarsData: [LineChartBarData(spots: points)]))),
+              ElevatedButton(
+                child: const Text('Stop Plot'),
+                onPressed: () {
+                  state.clearDevice();
+                },
+              )
+            ],
+          ))
+        : Container();
   }
 }
 
@@ -96,7 +151,7 @@ class QueryWidget extends StatelessWidget {
               decoration: const InputDecoration(
                   labelText: 'Device Name*', border: OutlineInputBorder()),
               onSubmitted: (String name) {
-                if (!name.isEmpty) {
+                if (name.isNotEmpty) {
                   state.setDevice(name.toUpperCase());
                 }
               },
